2233de16402cd9c61e30b7b229b2fb027bb7829c
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-08 18:24:56 +0300

    Add a PS (1) in sheet Data Types and Conditions

---
+++ ICPC Assiut  Sheets Version 3/Data type - Conditions - sh1/src/A.java
@@ -0,0 +1,15 @@
+ // Say Hello With C++
+
+import java.util.Scanner;
+
+public class A {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String s;
+        s = input.next();
+        System.out.println("Hello, " + s);
+
+    }
+
+}
