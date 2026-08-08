c2ed4a1ce9ec4ef576ecdda0daceda030566c418
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-08 17:44:36 +0300

    Add Main file to the sheet file

---
+++ ICPC Assiut  Sheets Version 3/Data type - Conditions - sh1/src/Main.java
@@ -0,0 +1,9 @@
+ // sheet 1 of ICPC Assiut sheets Data types and conditions
+
+public class Main {
+  public static void main(String[] args) {
+
+      System.out.println("Welcome ,sheet 1 of PS");
+
+  }
+}
