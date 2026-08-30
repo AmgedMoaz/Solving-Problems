c2a71c1b9d6669d881b404e46874f2e197c5af4a
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-30 23:45:22 +0300

    Add problem that be solved

---
+++ Codeforces/Files with Java/src/Two.java
@@ -0,0 +1,34 @@
+// Write a code to solve problem by using two pointers technique
+
+import java.util.Scanner;
+
+public class Two {
+    static public  void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        byte t;
+        t = input.nextByte();
+
+        while(t > 0) {
+            short n;
+            n = input.nextShort();
+            short ans = n;
+
+            String s;
+            s = input.next();
+            int left = 0 , right = n-1;
+            while(left <= right) {
+                if(s.charAt(left) != s.charAt(right)) {
+                    ans -= 2;
+                    left++;
+                    right--;
+                }else {
+                    break;
+                }
+            }
+            System.out.println(ans);
+            t--;
+        }
+    }
+}
