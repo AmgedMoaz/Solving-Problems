9e949fa50fcb5104275dc25b481fbf9942cc15c4
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-01 19:18:28 +0300

    Add problem that be solved

---
+++ Codeforces/Files with Java/src/Five.java
@@ -0,0 +1,34 @@
+// Write a code to solve problem by using Greedy Algorithm technique
+
+import java.util.Scanner;
+
+public class Five {
+    public static void main(String[] argv) {
+
+        Scanner input = new Scanner(System.in);
+
+        short t;
+        t = input.nextShort();
+
+        while(t > 0) {
+            int n , k;
+            n = input.nextInt();
+            k = input.nextInt();
+
+            String s;
+            s = input.next();
+
+            int i = 0 , operation = 0;
+            while(i < n){
+                if(s.charAt(i) == 'B') {
+                    operation++;
+                    i += k;
+                }else {
+                    i++;
+                }
+            }
+            System.out.println(operation);
+            t--;
+        }
+    }
+}
