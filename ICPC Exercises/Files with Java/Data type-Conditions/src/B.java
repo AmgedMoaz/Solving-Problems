77e12c94d97eac972c8b4a8194a57eb2514ee59f
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-03 14:44:32 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Data type-Conditions/src/B.java
@@ -0,0 +1,20 @@
+// Restricted
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String []argv) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if((a+b) < 10) {
+            System.out.println((a+b));
+        }else {
+            System.out.println("error");
+        }
+
+    }
+}
