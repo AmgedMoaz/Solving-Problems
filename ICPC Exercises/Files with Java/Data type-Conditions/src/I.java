3baa3b5f65b1ff07308bd5858ad500bab67943e0
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-03 18:03:22 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Data type-Conditions/src/I.java
@@ -0,0 +1,28 @@
+// Signed Difficulty
+
+import java.util.Scanner;
+
+public class I {
+    public static void main(String []argv) {
+
+        Scanner in = new Scanner(System.in);
+
+        String num = in.next();
+
+        // تقسيم النص عند النقطة (.)
+        String[] parts = num.split("\\.");
+
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+
+        if(y >= 0 && y <= 2) {
+            System.out.println(x+"-");
+        }else if(y >= 3 && y <= 6) {
+            System.out.println(x);
+        }else {
+            System.out.println(x+"+");
+        }
+        in.close();
+
+    }
+}
