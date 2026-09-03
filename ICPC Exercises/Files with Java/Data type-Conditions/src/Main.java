8ff9f26ae0e81791cb801b3b4cf540081af961cf
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-03 14:26:12 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Data type-Conditions/src/Main.java
@@ -0,0 +1,20 @@
+// Your First Judge
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+
+        String standard_phrase = "Hello,World!";
+
+        if(s.equals(standard_phrase)) {
+            System.out.println("AC");
+        }else {
+            System.out.println("WA");
+        }
+
+    }
+}
