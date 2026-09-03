14fcd80e98a71472157a15ca525531ce8c363b61
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-03 15:43:33 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Data type-Conditions/src/E.java
@@ -0,0 +1,17 @@
+// ABCxxx
+
+import java.util.Scanner;
+
+public class E {
+    public static void main(String []argv) {
+
+        Scanner input = new Scanner(System.in);
+
+        String round = "ABC";
+        int n = input.nextInt();
+
+        System.out.println(round+n);
+        input.close();
+
+    }
+}
