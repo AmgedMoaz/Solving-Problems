923acc59d2918f4b5b4b558e2585e2e766d5b062
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-04 17:23:53 +0300

    Add problem that be solved

---
+++ ICPC Assiut  Sheets Version 3/Loops-sh2/src/K.java
@@ -0,0 +1,19 @@
+// Divisors
+
+import java.util.Scanner;
+
+public class K {
+    public static void main(String []argv) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        for(int i = 1 ; i <= n ; i++) {
+            if(n%i == 0) {
+                System.out.println(i);
+            }
+        }
+        in.close();
+
+    }
+}
