22380bc45bfb72e228029676ce672d300ce13365
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-05 22:44:27 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Loops-sh2/src/Main.java
@@ -0,0 +1,32 @@
+// Find Multiple
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a , b , c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+
+        boolean isMultiple = false;
+        int i = a;
+        while(i <= b) {
+            if(i%c == 0) {
+                isMultiple = true;
+                break;
+            }
+            i++;
+        }
+        if(isMultiple) {
+            System.out.println(i);
+        }else {
+            System.out.println(-1);
+        }
+
+        in.close();
+    }
+}
