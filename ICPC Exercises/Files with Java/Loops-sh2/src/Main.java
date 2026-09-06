6eac17a5bc1bf65f17aeecf6a8d962f02af9d255
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-06 22:10:33 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Loops-sh2/src/Main.java
@@ -0,0 +1,31 @@
+// Utopian Tree
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int t;
+        t = in.nextShort();
+
+        while(t > 0) {
+            int n;
+            n = in.nextInt();
+
+            int height = 1;
+            for(int i = 1 ; i <= n ; i++) {
+                if(i%2 != 0) {
+                    height *= 2;
+                }else {
+                    height += 1;
+                }
+            }
+            System.out.println(height);
+            t--;
+        }
+        in.close();
+
+    }
+}
