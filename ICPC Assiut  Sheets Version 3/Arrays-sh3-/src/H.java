3867c7c5ce6865e07e11c1bc625c3279982aac81
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-08 16:59:39 +0300

    Add the problem that be solved

--- ICPC Assiut  Sheets Version 3/Arrays-sh3-/src/H.java
+++ ICPC Assiut  Sheets Version 3/Arrays-sh3-/src/H.java
@@ -12,14 +12,18 @@ public class H {
         for(int i = 0 ; i < n ; i++) {
             arr[i] = in.nextInt();
         }
-        // Bubble sort
+        // Selection sort
         for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < n-i-1 ; j++) {
-                if(arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+            int minIndex = i;
+            for(int j = i+1 ; j < n ; j++) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
+            }
+            if(minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
         }
         for(int i = 0 ; i < n ; i++) {
