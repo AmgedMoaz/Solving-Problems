1fc83011f7e21ecc3553e3bcec75c7ce19c497b3
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-08 17:42:33 +0300

    Add the problem that be solved

--- ICPC Assiut  Sheets Version 3/Arrays-sh3-/src/H.java
+++ ICPC Assiut  Sheets Version 3/Arrays-sh3-/src/H.java
@@ -12,19 +12,15 @@ public class H {
         for(int i = 0 ; i < n ; i++) {
             arr[i] = in.nextInt();
         }
-        // Selection sort
-        for(int i = 0 ; i < n ; i++) {
-            int minIndex = i;
-            for(int j = i+1 ; j < n ; j++) {
-                if(arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
+        // Insertion sort
+        for(int i = 1 ; i < n ; i++) {
+            int j = i-1;
+            int key = arr[i];
+            while(j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
             }
-            if(minIndex != i) {
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
-            }
+            arr[j+1] = key;
         }
         for(int i = 0 ; i < n ; i++) {
             System.out.print(arr[i] + " ");
