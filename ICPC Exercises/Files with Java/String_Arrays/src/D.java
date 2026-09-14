b0da3da6d6257c5c03028297b9f6c57bfe7613ff
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-14 17:32:03 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/String_Arrays/src/D.java
@@ -0,0 +1,26 @@
+// Gravity Flip
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class D {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        short n = in.nextShort();
+        short[] arr = new short[n];
+
+        for(int i = 0 ; i < n ; i++) {
+            arr[i] = in.nextShort();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0 ; i < n ; i++)
+            System.out.print(arr[i] + " ");
+
+        in.close();
+
+    }
+}
