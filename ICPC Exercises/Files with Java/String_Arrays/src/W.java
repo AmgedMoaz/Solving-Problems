4c95a3818ec84c727b9686f2bb321dc50ec562c3
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-16 13:39:09 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/String_Arrays/src/W.java
@@ -0,0 +1,17 @@
+// Word Capitalization
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class W {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String word = in.next();
+        String result = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+        System.out.println(result);
+
+        in.close();
+
+    }
+}
