200a55cc52e6b9c67b4e86c23ed17c90f43c5d34
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-17 20:13:40 +0300

    Add problem that be solved

---
+++ ICPC Assiut  Sheets Version 3/Recursion-sh7/src/I.java
@@ -0,0 +1,25 @@
+// Count Vowels
+
+import java.util.Scanner;
+
+public class I {
+    public static void main(String[] argv) {
+        Scanner in = new Scanner(System.in);
+
+        String phrase = in.nextLine();
+        int result = countVowels(phrase,0,0);
+        System.out.println(result);
+        in.close();
+    }
+    static int countVowels(String s , int counter , int index) {
+        if(index == s.length())
+            return counter;                           // base case
+
+        char c = s.charAt(index);
+        c = Character.toLowerCase(c);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                counter++;
+        }
+        return countVowels(s,counter,index+1);
+    }
+}
