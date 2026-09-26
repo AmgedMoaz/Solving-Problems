dd4a173415383b3fe01912a07f8b51964a38a884
Amged Moaz <amgedmoaz7@gmail.com>
2026-09-26 15:05:58 +0300

    Add problem that be solved

---
+++ ICPC Exercises/Files with Java/Time_and_Space-Complexity_plus_Functions/src/A.java
@@ -0,0 +1,17 @@
+// Again Twenty Five!
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        long n = in.nextLong();
+        long result = solve(n);
+        System.out.println(result);
+        in.close();
+    }
+    static int solve(long num) {
+        return 25;
+    }
+}
